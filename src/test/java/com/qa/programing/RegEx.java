d960abfea410e4a241eaeedb0f2c2ddd38e94544
Abdulla Suhail <abdulla.suhail@gmail.com>
2021-10-07 20:30:23 +0530

    test

--- src/test/java/com/qa/programing/RegEx.java
+++ src/test/java/com/qa/programing/RegEx.java
@@ -3,7 +3,6 @@ package com.qa.programing;
 public class RegEx {
 
 	public static void main(String[] args) {
-		
 		String str = "This#is$my%first^discussion@with&Altimetrik";
 		String s = str.replaceAll("[#$%^@&]", " ");
 		System.out.println(s);
